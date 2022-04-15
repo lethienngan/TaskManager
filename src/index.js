79344f9a7838ce390b175db0b3ddc629838f24fd
lethienngan <lethienngan95@gmail.com>
2022-04-15 08:37:46 +0700

    store data to localStorage by using redux-persist

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './redux/store';
+// import store froms './redux/store';
 import { Provider } from 'react-redux';
-import { saveState } from './redux/localStorage'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 
-store.subscribe(() => {
-    saveState(store.getState())
-})
+import { saveState } from './redux/localStorage'
+import { store } from './redux/store'
+
+// store.subscribe(() => {
+//     saveState(store.getState())
+// })
+let persistor = persistStore(store)
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
--- src/redux/store.js
+++ src/redux/store.js
@@ -9,17 +9,45 @@
 // export default store;
 
 import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from "redux";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import { filtersReducer } from '../components/Filters/FiltersReducer'
 import { todoListReducer } from '../components/TodoList/TodoListReducer'
-import { loadState } from './localStorage'
+// import { loadState } from './localStorage'
 
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage
+}
 
-const store = configureStore({
-    reducer: {
-        filters: filtersReducer.reducer,
-        todoList: todoListReducer.reducer,
-    },
-    preloadedState: loadState(),
+const rootReducer = combineReducers({
+    filters: filtersReducer.reducer,
+    todoList: todoListReducer.reducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+    // preloadedState: loadState(),
 });
 
-export default store;
+// export default store;
